2b99ae1e149ff85b8fa7e003410e04503f040f71
Manan2k4 <ghoniyamanan@gmail.com>
2025-05-29 11:35:59 +0530

    redmi

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -15,4 +16,9 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
