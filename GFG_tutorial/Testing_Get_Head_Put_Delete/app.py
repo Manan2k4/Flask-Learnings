421171375d15dbe42a2513759f93ce45055ff437
Manan2k4 <ghoniyamanan@gmail.com>
2025-05-31 11:17:01 +0530

    31/5/25

--- GFG_tutorial/Testing_Get_Head_Put_Delete/app.py
+++ GFG_tutorial/Testing_Get_Head_Put_Delete/app.py
@@ -1,6 +1,20 @@
-from flask import Flask, url_for, redirect, request
+from flask import Flask, url_for, redirect, request, render_template
 app = Flask(__name__)
 
 @app.route('/success/<name>')
 def success(name):
     return 'welcome %s' % name
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        if user:
+            return redirect(url_for('success', name=user))
+        else:
+            return "name not provided in POST", 400
+    else:
+        return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
