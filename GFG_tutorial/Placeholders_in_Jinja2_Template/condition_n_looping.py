1da911d91d5166a3762d3cc04cca802b2ba0789c
Manan2k4 <ghoniyamanan@gmail.com>
2025-06-02 10:07:06 +0530

    lambdas

--- GFG_tutorial/Placeholders_in_Jinja2_Template/condition_n_looping.py
+++ GFG_tutorial/Placeholders_in_Jinja2_Template/condition_n_looping.py
@@ -1,9 +1,14 @@
 from jinja2 import Environment, FileSystemLoader
+import os
 
-# Setup Jinja2 environment and tell it where to look for templates
-env = Environment(loader=FileSystemLoader('condition_n_looping'))
+# Get the current directory of the script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+template_dir = os.path.join(current_dir, 'condition_n_looping')
 
-# Load the main template (which extends base.html)
+# Setup Jinja2 environment
+env = Environment(loader=FileSystemLoader(template_dir))
+
+# Load the template
 template = env.get_template('index_template.html')
 
 # Variables to pass
@@ -11,9 +16,12 @@ name = 'Manan'
 email = 'Manan@example.com'
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-# Render the template with values
+# Render the template
 rendered_form = template.render(pl_name=name, pl_email=email, numbers=numbers)
 
-# Write the output to an HTML file
-with open('condition_n_looping/index.html', 'w') as output:
+# Write the output
+output_path = os.path.join(current_dir, 'index.html')  # Saves in main directory
+with open(output_path, 'w') as output:
     output.write(rendered_form)
+
+print(f"Template rendered successfully to {output_path}")
