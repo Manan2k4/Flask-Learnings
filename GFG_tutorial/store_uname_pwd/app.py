a482dc2ea7c09f067c2406f17b58777f40b1e590
Manan2k4 <ghoniyamanan@gmail.com>
2025-06-03 12:27:44 +0530

    tane mara sam aama n joto

--- GFG_tutorial/store_uname_pwd/app.py
+++ GFG_tutorial/store_uname_pwd/app.py
@@ -2,14 +2,15 @@ from flask import *
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
-from werkzeug.security import generate_password_hash, check_password_hash
 
+# initialize first flask
 app = Flask(__name__)
 app.secret_key = 'GeeksForGeeks'
 
+# Set MySQL data
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Manan2k4@MySQL'   # Use your MySQL password
+app.config['MYSQL_PASSWORD'] = 'Manan2k4@MySQL'   # Use you mysql password 
 app.config['MYSQL_DB'] = 'user_table'
 
 mysql = MySQL(app)
@@ -27,8 +28,10 @@ def create_table():
     mysql.connection.commit()
     cursor.close()
 
+# Call the function when the app starts
 with app.app_context():
     create_table()
+
 
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
@@ -38,25 +41,32 @@ def login():
         email = request.form['email']
         password = request.form['password']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM user WHERE email = %s', (email,))
+        cursor.execute(
+            'SELECT * FROM user WHERE email = % s AND password = % s', 
+                  (email, password, ))
         user = cursor.fetchone()
-        if user and check_password_hash(user['password'], password):
+        if user:
             session['loggedin'] = True
             session['userid'] = user['userid']
             session['name'] = user['name']
             session['email'] = user['email']
-            message = 'Logged in successfully!'
-            return render_template('user.html', message=message)
+            message = 'Logged in successfully !'
+            return render_template('user.html', 
+                                   message=message)
         else:
-            message = 'Please enter correct email / password!'
-    return render_template('login.html', message=message)
+            message = 'Please enter correct email / password !'
+    return render_template('login.html', 
+                           message=message)
 
+
+# Make function for logout session
 @app.route('/logout')
 def logout():
     session.pop('loggedin', None)
     session.pop('userid', None)
     session.pop('email', None)
     return redirect(url_for('login'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -66,22 +76,25 @@ def register():
         password = request.form['password']
         email = request.form['email']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM user WHERE email = %s', (email,))
+        cursor.execute('SELECT * FROM user WHERE email = % s', (email, ))
         account = cursor.fetchone()
         if account:
-            message = 'Account already exists!'
+            message = 'Account already exists !'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            message = 'Invalid email address!'
+            message = 'Invalid email address !'
         elif not userName or not password or not email:
-            message = 'Please fill out the form!'
+            message = 'Please fill out the form !'
         else:
-            hashed_password = generate_password_hash(password)
-            cursor.execute('INSERT INTO user VALUES (NULL, %s, %s, %s)', (userName, email, hashed_password))
+            cursor.execute(
+                'INSERT INTO user VALUES (NULL, % s, % s, % s)', 
+                      (userName, email, password, ))
             mysql.connection.commit()
-            message = 'You have successfully registered!'
+            message = 'You have successfully registered !'
     elif request.method == 'POST':
-        message = 'Please fill out the form!'
+        message = 'Please fill out the form !'
     return render_template('register.html', message=message)
 
+
+# run code in debug mode
 if __name__ == "__main__":
     app.run(debug=True)
